069b630562730582d3a7811e6a5fadca78354d6c
Hunter Tiner <Hulatin@gmail.com>
2021-06-07 21:17:29 -0500

    update

--- Python/getLearnt.py
+++ Python/getLearnt.py
@@ -8,40 +8,42 @@ chemEvents = pd.read_csv(r'Python\eventsOutput\events\2021Jun07_MQ2_0.1_10_90_Ev
 
 ignoreChem = 'Cyclohexane'
 numSplits = 2
-testSize = 0.1
+testSize = 0.3
 loops = 10
 
 gnb =  GaussianNB()
 
+# split string, selects chemical name (drops the numbering)
+chemEvents['chemical'] = chemEvents['chemical'].str.split("-").str[0]
+# rename column
+chemEvents = chemEvents.rename(columns={"chemical": "pred"})
+
+chemEvents = chemEvents[~chemEvents.pred.str.contains(ignoreChem)]
+
 colList = ["Temp_C*", "Humidity",  "Gas_ohms", "Pressure_pa"]
-for i in colList:
+for i in range(0, len(colList)):
         
     ignoreCol = colList[i]
     #ignoreCol = 'Gas_ohms'
     ignoreCol = '^', ignoreCol
     ignoreCol = ''.join(ignoreCol)
-
-    # split string, selects chemical name (drops the numbering)
-    chemEvents['chemical'] = chemEvents['chemical'].str.split("-").str[0]
-    # rename column
-    chemEvents = chemEvents.rename(columns={"chemical": "pred"})
-
-    chemEvents = chemEvents[~chemEvents.pred.str.contains(ignoreChem)]
-    chemEvents = chemEvents.loc[:,~chemEvents.columns.str.contains(ignoreCol)]
+    
+    chemDF = chemEvents.loc[:,~chemEvents.columns.str.contains(ignoreCol)]
 
     # groups by class/chemical, downsamples to balance the dataset
-    g = chemEvents.groupby('pred')
-    chemEvents = pd.DataFrame(g.apply(lambda chemEvents: chemEvents.sample(g.size().min()).reset_index(drop=True)))
-    
+    g = chemDF.groupby('pred')
+    chemDF = pd.DataFrame(g.apply(lambda chemDF: chemDF.sample(g.size().min())))
+    chemDF = chemDF.reset_index(drop=True)
+       
     x = 0
     while(1):
         x = x + 1
-    
+  
         split = StratifiedShuffleSplit(n_splits=numSplits, test_size=testSize, random_state=random.seed())
         
-        for train_index, test_index in split.split(chemEvents, chemEvents['pred']):
-            strat_train_set = chemEvents.loc[train_index]
-            strat_test_set = chemEvents.loc[test_index]
+        for train_index, test_index in split.split(chemDF, chemDF['pred']):
+            strat_train_set = chemDF.loc[train_index]
+            strat_test_set = chemDF.loc[test_index]
 
         y_train = strat_train_set['pred']
         x_train = strat_train_set.iloc[:,1:strat_train_set.shape[1]]
@@ -53,4 +55,5 @@ for i in colList:
         print("Number of mislabeled points out of a total %d points : %d" % (x_test.shape[0], (y_test != y_pred).sum()))   
         
         if x == loops:
+            print('\n')
             break
